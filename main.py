a097b5f72d12ca3c3859044f58eb5ce2ee44e42e
Nick Anderegg <nick.anderegg@zymbit.com>
2021-03-10 18:02:56 -0500

    feat: parse the files to be rendered for Hugo
    Signed-off-by: Nick Anderegg <nick@anderegg.io>

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os, sys
 import pathlib
+from pprint import pprint
 
 
 def main():
-    input_dir = pathlib.Path(os.environ["INPUT_PATH"])
+    input_dir = pathlib.Path(os.environ["INPUT_RAWPATH"])
+    output_dir = pathlib.Path(os.environ["INPUT_OUTPUTPATH"])
 
+    file_list = []
     for f in input_dir.iterdir():
-        print(f)
+        base_file_name = f.name.removeprefix("GENERATED_")
+        output_file = output_dir / base_file_name
+
+        file_list.append((f.resolve(), output_file.resolve()))
 
     print(f"::set-output name=testOutput::This is my test output.")
 
