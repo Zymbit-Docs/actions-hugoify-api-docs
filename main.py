3f9a41e1c71c3ad4f89ac1c2d3aacc65d0ca7632
Nick Anderegg <nick.anderegg@zymbit.com>
2021-03-15 23:08:35 -0400

    feat: correct indentation and overlapping emphasis when parsing
    Signed-off-by: Nick Anderegg <nick@anderegg.io>

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import os, sys
 import pathlib
+import re
+import io
 from datetime import datetime
 
-from typing import List
+from typing import List, Any
 
 # import frontmatter
 from ruamel.yaml import YAML
@@ -168,8 +170,83 @@ def parse_content(input_file: pathlib.Path) -> str:
     str
         A string containing the parsed and reformatted doc contents.
     """
+
     with input_file.open("r") as f:
-        file_content = f.readlines()
+        file_content_str = f.read()
+
+    bolded_item = r"\*{2}(?P<heading>[a-z ]+)\*{2}"
+    section = (
+        rf"^\* {bolded_item}\n+"
+        # rf"[ ]{{4}}(?P<list_heading>\* \*\*[a-z0-9_]+\*\* .+\n\n)"
+        rf"(?P<section_body>(([ ]{{2,4}})+.+\n\n*)+)"
+    )
+
+    subsection = (
+        rf"[ ]{{4}}\* \*\*(?P<list_item_name>.*)\*\* (?P<list_item_body>.*)\n"
+        rf"(?P<grouped_lines>(?:(?:[ ]{{4}}(?:[^*]|\*\*).*\n)|\n)*)"
+    )
+
+    matches = re.finditer(
+        section,
+        file_content_str,
+        re.IGNORECASE | re.MULTILINE,
+    )
+
+    full_content = []
+    last_match_end = -1
+    for match in matches:
+        full_content += file_content_str[last_match_end + 1 : match.start()].split("\n")
+        last_match_end = match.end()
+
+        subsection_content = match.group(0)
+
+        groups = match.groupdict()
+
+        new_content = [f"#### {groups['heading']}\n"]
+
+        if groups["heading"] == "Parameters":
+            submatches = re.finditer(
+                subsection,
+                groups["section_body"],
+                re.IGNORECASE | re.MULTILINE,
+            )
+
+            for submatch in submatches:
+                submatch_groups = submatch.groupdict()
+                new_content.append(
+                    f"- `{submatch_groups['list_item_name']}` {submatch_groups['list_item_body']}"
+                )
+
+                grouped_lines = submatch_groups["grouped_lines"].split("\n")
+                while grouped_lines and not grouped_lines[-1]:
+                    del grouped_lines[-1]
+
+                final_lines = []
+                for line in grouped_lines:
+                    final_lines.append(line.replace("        ", "    "))
+
+                new_content += final_lines
+                new_content.append("")
+
+        else:
+            body_lines = groups["section_body"].split("\n")
+            while body_lines and not body_lines[-1]:
+                del body_lines[-1]
+
+            final_lines = []
+            for line in body_lines:
+                final_lines.append(line.replace("    ", "  "))
+
+            new_content += final_lines
+            new_content.append("")
+
+        full_content += new_content
+
+    merged_content = "\n".join(full_content)
+
+    file_content = io.StringIO(merged_content).readlines()
+
+    print("Length of content:", len(file_content))
 
     first_pass = []
     last_was_newline = False
@@ -179,6 +256,17 @@ def parse_content(input_file: pathlib.Path) -> str:
 
     if file_content[1] != "\n":
         first_pass.append(file_content[1])
+
+    # * **src** (*Union**[**str**, **bytes**]*) –
+    bad_italics_pattern: Any = r"(?P<prefix>[ ]*[*-] )`(?P<param_name>.+?)` \(\*(?P<type_op>[a-z]+)\*\*(?P<type_vals>\[.*?\])\*\)(?P<remaining>.*)"
+    bad_italics_pattern = re.compile(bad_italics_pattern, re.IGNORECASE)
+
+    # # * **Parameters**
+    # subheading_pattern: Any = r"\* \*\*(Parameters|Return type|Returns|Raises|.+)\*\*"
+    # subheading_pattern = re.compile(subheading_pattern, re.IGNORECASE)
+
+    # four_space_pattern: Any = r"([ ]{4})+(\* \*{2}.+)"
+    # four_space_pattern = re.compile(four_space_pattern, re.IGNORECASE)
 
     combined_line: List[str] = []
     for line in file_content[2:]:
@@ -193,16 +281,32 @@ def parse_content(input_file: pathlib.Path) -> str:
         else:
             last_was_newline = False
 
-            second_level_ol = "    * "
-            if line.startswith(second_level_ol):
-                line = line.replace(second_level_ol, "    - ")
+            result = bad_italics_pattern.match(line)
+            if result:
+                groups = result.groupdict()
+                line = f"{groups['prefix']}`{groups['param_name']}` (<em>{groups['type_op']}{groups['type_vals']}</em>){groups['remaining']}"
+
+            # result = subheading_pattern.match(line)
+            # if result:
+            #     line = f"##### {result.group(1)}"
+
+            # result = four_space_pattern.match(line)
+            # if result:
+            #     num_groups = len(result.groups())
+            #     indent = "  " * (num_groups - 1)
+            #     line = f"{indent}{result.group(num_groups)}"
+            #     # print(line)
+            #     # print(result.groups(), line.rstrip())
+            #     # line = f"##### {result.group(1)}"
 
             # parsed_content.append(line)
             if not combined_line:
                 combined_line.append(line.rstrip())
+                # combined_line.append("".join(line.split("\n")))
             else:
-                combined_line.append(line.strip())
+                combined_line.append(line.strip(" \n"))
 
+    # pprint(first_pass[:200])
     second_pass = []
     for line in first_pass:
         if line.startswith("##"):
@@ -211,6 +315,7 @@ def parse_content(input_file: pathlib.Path) -> str:
 
         second_pass.append(line)
 
+    print("Lines at end of second pass:", len(second_pass))
     return "".join(second_pass)
 
 
